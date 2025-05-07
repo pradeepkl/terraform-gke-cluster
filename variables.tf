58f90f1e0d50762cc47690060870fa1b912dec43
Pradeep <pradeep.kumar44@gmail.com>
2025-05-07 04:00:28 +0000

    committing the gke cluster terraform script

---
+++ variables.tf
@@ -0,0 +1,7 @@
+# terraform.tfvars
+
+project_id   = "your-gcp-project-id"  # Replace with your GCP project ID
+cluster_name = "optum-gke-cluster"
+network      = "default"              # Replace if using custom VPC
+subnetwork   = "default"              # Replace if using custom subnet
+environment  = "production"
